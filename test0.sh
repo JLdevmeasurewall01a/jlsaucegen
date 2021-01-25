585c7e7aa7d638b3d1754cb7dbe2a64be5c52e82
jl_devMSRWall01a <Minelolpjl@gmail.com>
2021-01-25 02:20:43 +0000

    fixed the sh script

--- test0.sh
+++ test0.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 echo 'well here we go'
-python3 saucegen.py
+echo "F" | python3 saucegen.py
 sleep 3
-python3 saucegen.py
+echo "F" | python3 saucegen.py
 sleep 3
-python3 saucegen.py
+echo "F" | python3 saucegen.py
 sleep 3 
-python3 saucegen.py
+echo "F" | python3 saucegen.py
 sleep 3 
-python3 saucegen.py
+echo "F" | python3 saucegen.py
 sleep 3 
-python3 saucegen.py
+echo "F" | python3 saucegen.py
 sleep 3 
-python3 saucegen.py
+echo "F" | python3 saucegen.py
 sleep 3 
-python3 saucegen.py
+echo "F" | python3 saucegen.py
 sleep 3 
-python3 saucegen.py
+echo "F" | python3 saucegen.py
 sleep 3 
-python3 saucegen.py
+echo "F" | python3 saucegen.py
 sleep 3 
-python3 saucegen.py
+echo "F" | python3 saucegen.py
 sleep 3 
 
 echo 'done!'
